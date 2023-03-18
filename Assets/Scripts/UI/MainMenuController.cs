ea0d114e436090115aa78f33422742d4496a9b67
Tairos <tairos21@gmail.com>
2023-03-18 23:12:59 +0100

    First draft of UI and XRInteraction

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnPlayClicked()
+    {
+        Debug.Log("PLAY");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+    }
+}
