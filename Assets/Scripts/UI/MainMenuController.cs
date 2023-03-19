e43eb6620193a98d5019c2fd6d75ecce2bef07b7
Tairos <tairos21@gmail.com>
2023-03-19 09:42:43 +0100

    Animate menu

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] PlayableDirector _showMenuDirector;
+    [SerializeField] PlayableDirector _hideMenuDirector;
+
     public void OnPlayClicked()
     {
-        Debug.Log("PLAY");
+        Hide();
     }
 
     public void OnQuitClicked()
@@ -14,5 +18,19 @@ public class MainMenuController : MonoBehaviour
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #endif
+    }
+
+    public void Hide()
+    {
+        _showMenuDirector.Stop();
+        _hideMenuDirector.time = 0;
+        _hideMenuDirector.Play();
+    }
+
+    public void Show()
+    {
+        _hideMenuDirector.Stop();
+        _showMenuDirector.time = 0;
+        _showMenuDirector.Play();
     }
 }
